51c08e89479d9cb126db9efbe838609d31583f97
Gustavo <go.gusfm@gmail.com>
2020-01-04 21:48:25 -0300

    Add first FT using unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert "Django" in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        #Ana ouviu falar de uma aplicaco online interessante
+        #para lista de tarefas, ela decidiu verificar
+        self.browser.get('http://localhost:8000')
+
+        #Ela percebe que o título da página menciona "to-do"
+        self.assertIn ("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        #Ela é convidada a inserir um item de tarefa imediatamente
+
+        #Ela digita "Buy peacock feathers" em uma caixa de texto
+        #(Seu hobby é fazer iscas para pescar)
+
+        #Quando ela tecla enter a página é atualizada, agora a página lista
+        #"1: Buy peacock feathers" como um item da lista de tarefas
+
+        #Ainda continua havendo uma caixa de texto para acrescentar outro item.
+        #ELa insere "User peacock feathers to make a fly"
+
+        #A página é atualizada novamente e agora mostra dois itens em sua lista
+
+        #Ana se pergunta se o site lembrará de sua lista. Ela nota que o site
+        #gerou um URL único para ela - existe um texto explicativo.
+
+        #Ela acessa essa URL - sua lista continua lá.
+
+        #Satisfeita ela volta a dormir.
+
+if __name__ == '__main__':
+    unittest.main(warnings="ignore")
