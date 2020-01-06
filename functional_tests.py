a2bc0a88532ff668ee5f6d3e5354821411171029
gustavuka <go.gusfm@gmail.com>
2020-01-06 01:04:59 -0300

    Run black

--- functional_tests.py
+++ functional_tests.py
@@ -5,7 +5,6 @@ import unittest
 
 
 class NewVisitorTest(unittest.TestCase):
-
     def setUp(self):
         self.browser = webdriver.Firefox()
 
@@ -13,44 +12,45 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        #Ana ouviu falar de uma aplicaco online interessante
-        #para lista de tarefas, ela decidiu verificar
-        self.browser.get('http://localhost:8000')
+        # Ana ouviu falar de uma aplicaco online interessante
+        # para lista de tarefas, ela decidiu verificar
+        self.browser.get("http://localhost:8000")
 
-        #Ela percebe que o título da página menciona "to-do"
-        self.assertIn ("To-Do", self.browser.title)
+        # Ela percebe que o título da página menciona "to-do"
+        self.assertIn("To-Do", self.browser.title)
         header_text = self.browser.find_element_by_tag_name("h1").text
         self.assertIn("To-Do", header_text)
 
-        #Ela é convidada a inserir um item de tarefa imediatamente
+        # Ela é convidada a inserir um item de tarefa imediatamente
         input_box = self.browser.find_elements_by_id("id_new_item")
         self.assertEqual(input_box.get_attribute("placeholder"), "Enter a to-do item")
 
-        #Ela digita "Buy peacock feathers" em uma caixa de texto
-        #(Seu hobby é fazer iscas para pescar)
+        # Ela digita "Buy peacock feathers" em uma caixa de texto
+        # (Seu hobby é fazer iscas para pescar)
         input_box.send_keys("Buy peacock feathers")
 
-        #Quando ela tecla enter a página é atualizada, agora a página lista
-        #"1: Buy peacock feathers" como um item da lista de tarefas
+        # Quando ela tecla enter a página é atualizada, agora a página lista
+        # "1: Buy peacock feathers" como um item da lista de tarefas
         input_box.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_elements_by_id("id_list_table")
         rows = table.find_elements_by_tag_name("tr")
-        self.assertTrue(any(row.text == "1: Buy peacock feathers"  for row in rows))
+        self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows))
 
-        #Ainda continua havendo uma caixa de texto para acrescentar outro item.
-        #ELa insere "User peacock feathers to make a fly"
+        # Ainda continua havendo uma caixa de texto para acrescentar outro item.
+        # ELa insere "User peacock feathers to make a fly"
         self.fail("Finish the test!!")
 
-        #A página é atualizada novamente e agora mostra dois itens em sua lista
+        # A página é atualizada novamente e agora mostra dois itens em sua lista
 
-        #Ana se pergunta se o site lembrará de sua lista. Ela nota que o site
-        #gerou um URL único para ela - existe um texto explicativo.
+        # Ana se pergunta se o site lembrará de sua lista. Ela nota que o site
+        # gerou um URL único para ela - existe um texto explicativo.
 
-        #Ela acessa essa URL - sua lista continua lá.
+        # Ela acessa essa URL - sua lista continua lá.
 
-        #Satisfeita ela volta a dormir.
+        # Satisfeita ela volta a dormir.
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main(warnings="ignore")
--- lists/tests.py
+++ lists/tests.py
@@ -6,8 +6,8 @@ from lists.views import home_page
 
 # Create your tests here.
 
-class HomePageTest(TestCase):
 
+class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home_page)
